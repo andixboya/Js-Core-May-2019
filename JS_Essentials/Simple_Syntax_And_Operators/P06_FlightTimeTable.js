3a80a844fab77af18e8f1e336a19410067fcf4e5
andixboya <andreyanboyadzhiev@gmail.com>
2019-05-28 08:53:55 +0300

    deleted

--- JS_Essentials/Simple_Syntax_And_Operators/P06_FlightTimeTable.js
+++
@@ -1,8 +0,0 @@
-function solve(input){
-
-    console.log(`${input[0]}: Destination - ${input[1]}, Flight - ${input[3]}, Time - ${input[2]}, Gate - ${input[4]}`);
-
-}
-// solve(
-//     ['Departures', 'London', '22:45', 'BR117', '42']
-// )
