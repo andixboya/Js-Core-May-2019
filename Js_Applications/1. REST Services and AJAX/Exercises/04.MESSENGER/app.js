d57791d4912f69aabe31cb13606ca2959240df95
andixboya <andreyanboyadzhiev@gmail.com>
2019-07-13 08:09:05 +0300

    rearranging a files + upload of 1 exercise

---
+++ Js_Applications/1. REST Services and AJAX/Exercises/04.MESSENGER/app.js
@@ -0,0 +1,11 @@
+function attachEvents() {
+    const author= document.querySelector(`#author`);
+    const content=document.querySelector(`#content`);
+    
+    
+    console.log(author);
+    console.log(content);
+    console.log('TODO...');
+}
+
+attachEvents(); 
